636b4d5f4b06eccc2bda795f0c49242a3290daae
Robert Brotherus <robert.brotherus@napa.fi>
2014-12-09 13:14:10 +0200

    (line length) (triangle-area)

--- ship.clj
+++ ship.clj
@@ -1,6 +1,24 @@
 (ns ModernClassic
   (:use clojure.test))
 
+;-------------------- Tools ------------------------
+
+(defn float= [x y]
+  (<= (Math/abs (- x y)) 0.0000001))
+
+(defn sqr [x] (* x x))
+
+(defn line-length [ p1 p2 ]
+  (Math/sqrt (reduce + (map (fn [a b] (sqr (- a b))) (vals p1) (vals p2)))))
+
+(defn triangle-area [ [ a b c ] ]
+  (let [ sides [ [a b] [a c] [b c] ]
+         side-lengths (map (fn [ [p1 p2] ] (line-length p1 p2)) sides)
+         perimeter (/ (reduce + side-lengths) 2) ]
+      (Math/sqrt (* perimeter (reduce * (map #(- perimeter %) side-lengths))))))
+
+;-------------------------------------------------------
+
 (defn invert-y [ point ] (assoc point :y (- (get point :y 0))))
 
 (defn make-hull-points [ { total-length :total-length width :width height :height
@@ -40,6 +58,11 @@
     :bow-length-percentage 22 :stern-length-percentage 10 } )
 
 ;; ---------------- Tests -------------------------
+
+(deftest test-math
+  (is (= (Math/sqrt 3) (line-length {:x 1 :y 1 :z 1} {:x 2 :y 2 :z 2 })))
+  (is (= 0.0 (triangle-area [ {:x 1 :y 1 :z 1} {:x 2 :y 2 :z 2} {:x 3 :y 3 :z 3} ] )))
+  (is (float= 0.5 (triangle-area [ {:x 0 :y 0 } {:x 1 :y 0 } {:x 0 :y 1 } ] ))))
 
 (deftest test-helpers
   (is (= { :x 5.5 :y -6.6 :z 7.7 } (invert-y { :x 5.5 :y 6.6 :z 7.7 })))
